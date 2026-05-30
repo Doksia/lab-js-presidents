14771a2dcc39256d1f6416ae271a4a74860aeb21
Miguel Romero <miguel.insti2@gmail.com>
2026-05-30 14:28:32 +0100

    solved lab

--- src/presidents.js
+++ src/presidents.js
@@ -419,29 +419,26 @@ const presidents = [
 
 
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
-
-
-
-
+function getNames(presidentsArr) {
+  return presidentsArr.map(president => president.name);
+}
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
-
-
-
-
+function getDemocraticPresidents(presidentsArr) {
+  return presidentsArr.filter(president => president.party === "Democratic");
+}
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
-
-
-
-
+function  countYearsInOffice(presidentsArr) {
+ return presidentsArr.reduce((accumulator, president) => {
+    if (presidents.leftOffice === "null"){
+      return accumulator;
+    } else {
+      return accumulator + (president.tookOffice - president.leftOffice);
+    }},0);
+}
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
-
-
-
-
+function sortPresidentsByBirthYear(presidentsArr) {
+  return presidentsArr.sort((currentPresident, nextPresident)=>currentPresident.birthYear - nextPresident.birthYear);
+}
 // Bonus: Iteration 5 | Age at Inauguration - `map()`
 function getAgeAtInauguration(presidentsArr) {}
 
